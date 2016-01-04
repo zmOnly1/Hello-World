b9b8dbbd698e4f0d15f27b24567bd70351ec6b01
zmOnly1 <406312944@qq.com>
2016-01-04 11:16:10 +0800

    branch1 hot fix

--- src/main/java/com/ds/HelloWorld.java
+++ src/main/java/com/ds/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Branch1");
+
+        System.out.println("hot fix");
     }
 }
