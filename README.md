0544f5fec969364917ceef8598ddaeec6c54bd87
zmOnly1 <406312944@qq.com>
2018-05-28 17:32:19 +0800

    commit 4

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Hello-World
+# Modules-Dependent-Test
 My First Project
---
+++ module2/src/main/java/com/ds/test/Main.java
@@ -0,0 +1,15 @@
+package com.ds.test;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = Lists.newArrayList();
+
+
+        System.out.println(list.size());
+    }
+}
